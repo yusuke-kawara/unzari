4449a8b139c3972467fc1ac775fe0a3ffff6fe6e
yusuke-kawara <happywanwan2007@gmail.com>
2024-05-25 11:53:54 +0900

    アセットのプリコンパイルがうまくいかない 　application.scssからapplication.scssに名前を戻しconfig/initializers/assets.rbに Rails.application.config.assets.precompile += %w( application.scss ) を追記 プリコンパイルする際application.scssを探しに行くことを設定する ✖️ なのでmanifest.jsに //= link application.scssを記述しプリコンパイルの際application.scssを探しに行くように設定する ✖️ config/application.rbに config.assets.css_compressor = :sass を追記してSprocketsがSCSSファイルを認識するようにする

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,6 @@ module App
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.assets.css_compressor = :sass
   end
 end
